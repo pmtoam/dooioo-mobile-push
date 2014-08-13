12a36868aa725718eb90800d8fd3c5548b1d9627
pmtoam <pmtoam@qq.com>
2014-08-13 11:20:12 +0800

    add new function with msgContent.

--- dooioo-mobile-push/src/com/dooioo/mobile/push/AxingDooiooMobilePush.java
+++ dooioo-mobile-push/src/com/dooioo/mobile/push/AxingDooiooMobilePush.java
@@ -66,6 +66,70 @@ public final class AxingDooiooMobilePush
 		return -1;
 	}
 	
+	/**
+	 * 推送一条消息到【我的客户】
+	 * @param employeeCode 员工号
+	 * @param msgCode 消息类型代号,用于判断是什么类型的消息
+	 * @param msgContent 消息内容(限制120字)
+	 * @return 返回值只作为调试，实际调用中，直接调用此方法，可忽略任何返回值：<br>
+	 * 1：推送成功<br>
+	 * -1：推送失败 [查询不到绑定的ID]<br>
+	 * -2：推送失败 [客户端没有绑定]<br>
+	 * -3：推送失败 [employeeCode null]<br>
+	 * -4：推送失败 [msgContent null]<br>
+	 * -5：推送失败 [msgContent长度  > 120]<br>
+	 */
+	public static int pushMessageOneOnlyUser(String employeeCode, MessageCode msgCode, String msgContent)
+	{
+		if (StringUtils.isEmpty(msgContent))
+		{
+			return -4;
+		}
+		
+		if (msgContent.length() > 120)
+		{
+			return -5;
+		}
+		
+		if (StringUtils.isEmpty(employeeCode.trim()))
+		{
+			return -3;
+		}
+		
+		String[] ids = getEmployeeIds(employeeCode);
+		
+		if (ids != null && ids.length == 2)
+		{
+			String userId = ids[0];
+			String channelId = ids[1];
+			
+			PushClient client = new DefaultPushClient();
+			PushMessageRequest request = new PushMessageRequest();
+			request.setUserId(userId);
+			request.setChannelId(channelId);
+			request.setMessageType(MessageType.notify);
+			String message = String.format("{'title':'%s'," + "'description':'%s',"
+					+ "'notification_builder_id':0,"
+					+ "'notification_basic_style':7," + "'open_type':0,"
+					+ "'custom_content':{'msgCode':'" + msgCode + "','msgContent':' " + msgContent + " '}}", "消息提醒", "您收到一条新消息");
+			request.setMessages(message);
+			request.setPushType(PushType.single_user);
+			request.setMessageKeys("nonKeys");
+			try
+			{
+				client.pushMessage(request);
+			}
+			catch (Exception e)
+			{
+				e.printStackTrace();
+				return -2;
+			}
+			return 1;
+		}
+		
+		return -1;
+	}
+	
 	private static String[] getEmployeeIds(String employeeCode)
 	{
 		String urlStr = "http://myclients.duapp.com/mysqlquery/employeeinfo?employeeCode=" + employeeCode;
@@ -92,25 +156,4 @@ public final class AxingDooiooMobilePush
 		return ids;
 	}
 	
-//	/**
-//	 * 
-//	 * @param userId
-//	 * @param channelId
-//	 * @param message
-//	 * @param messageKeys
-//	 */
-//	public static PushResponse<Integer> pushMessageOneOnlyUser(String userId,
-//			String channelId, String message, String messageKeys)
-//	{
-//		PushClient client = new DefaultPushClient();
-//		PushMessageRequest request = new PushMessageRequest();
-//		request.setUserId(userId);
-//		request.setChannelId(channelId);
-//		request.setMessageType(MessageType.notify);
-//		request.setMessages(message);
-//		request.setPushType(PushType.single_user);
-//		request.setMessageKeys(messageKeys);
-//		PushResponse<Integer> response = client.pushMessage(request);
-//		return response;
-//	}
 }
